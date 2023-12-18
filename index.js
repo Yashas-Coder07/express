a31de566341afc2ed885b03eaea97a3de02d1d2e
Yashas-Coder <76394152+Yashas-Coder07@users.noreply.github.com>
2023-12-18 20:33:00 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware");
-    next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="Size"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log("In the middleware");
-    res.send('<h1>Hello form express</h1>');
-})
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express!</h1>');
+});
 
 app.listen(3001);
